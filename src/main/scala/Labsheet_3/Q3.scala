21c2d694e4efef59f514dec4ce0b8bfb74eb8b0c
Mr. I.N.WEERASEKERA <121543346+inuka007@users.noreply.github.com>
2023-07-04 10:32:20 +0530

    Update Q3.scala

--- src/main/scala/Labsheet_3/Q3.scala
+++ src/main/scala/Labsheet_3/Q3.scala
@@ -1,20 +1,30 @@
+
 package Labsheet_3
 
-import scala.io.StdIn.readInt
 
-object Q3 {
+object Q3{
 
-  def toUpper(x: String):String = x.toUpperCase;
-  def toLower(x: String):String = x.toLowerCase;
-  def formatNames(name: String)(formatter: String => String):String = formatter(name);
+  def toUpper(x: String): String = x.toUpperCase()
+  def toLower(x: String): String = x.toLowerCase()
+
+  def formatNames(name: String, format: String): String = {
+    format match {
+      case x if x=="alluppercase" => toUpper(name)
+      case x if x=="First2Uppercase" => toUpper(name.substring(0, 2)) + toLower(name.substring(2,name.length))
+      case x if x=="alllowercase" => toLower(name)
+      case x if x=="firstandlastupper" => toUpper(name.head.toString) + toLower(name.substring(1,name.length-1)) + toUpper(name.substring(name.length-1))
+    }
+  }
 
   def main(args: Array[String]): Unit = {
-    println(formatNames("Benny")(toUpper))
-    println(formatNames("Niroshan")(toUpper))
-    println(formatNames("Saman")(toLower))
-    println(formatNames("Kumara")(toUpper))
-
-    }
-
+    var names: List[String] = List("Benny", "Niroshan", "Saman", "Kumara")
+    var formats: List[String] = List("alluppercase", "First2Uppercase", "alllowercase", "firstandlastupper")
+    var i: Int = 0
+    while (i < names.length) {
+      println("original : " + names(i) + " => After format : " + formatNames(names(i), formats(i)))
+      i = i + 1
+    }: Unit
+  }
 
 }
+
